b38e6112c64beb5d54ac02b237bdbe7af4f13dc1
zDeVolTz <vlad.pinti@gmail.com>
2024-02-15 16:23:01 +0200

    add script

---
+++ main.js
@@ -0,0 +1,24 @@
+function fnBrowserDetect(){
+                 
+    let userAgent = navigator.userAgent;
+    let browserName;
+    
+    if(/chrome.*opr|opera/i.test(userAgent)){
+        browserName = "Opera";
+      } else if(/chrome.*edg/i.test(userAgent)){
+        browserName = "Microsoft Edge";
+      } else if(/chrome|chromium|crios/i.test(userAgent)){
+        browserName = "Google Chrome";
+      } else if(/firefox/i.test(userAgent)){
+        browserName = "Firefox";
+      } else if(/safari/i.test(userAgent)){
+        browserName = "Safari";
+      }
+      else{
+        browserName="Кто ты монстр :)";
+      }
+    
+     document.querySelector("h1").innerText="You are using "+ browserName +" browser";         
+}
+
+fnBrowserDetect()
